fc06da2cd3d91c893d8499665008ea60eeea20c7
Qaiser Shehzad <qaiser@Qaisers-MacBook-Pro.local>
2020-02-17 12:13:44 +0500

    basic testing setup and linter

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "react-app",
+    "rules" : {
+        "indent": ["error", 4], // A custom style-related rule for example
+        "semi": 0,
+        "no-console": "error",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        // More custom rules here
+    }
+}
